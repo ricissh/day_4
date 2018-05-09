e07dff303f6ee4c033428ba6610b1428e578892b
ricissh <rihards.stegmanis@icloud.com>
2018-05-09 21:34:51 +0300

    vieglak neka if
    sapratu labi

--- day_4/day_4/Program.cs
+++ day_4/day_4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace day_4
 {
@@ -6,59 +7,101 @@ namespace day_4
     {
         static void Main(string[] args)
         {
-            int mainigais = 3;
-            string b;
-            int c;
-            int a;
-            b = Console.ReadLine();
-            a = Convert.ToInt16(b);
-           
+            int mainigais;
+            string ievade = Console.ReadLine();
+            mainigais = Convert.ToInt16(ievade);
 
-            mainigais = Convert.ToInt16("5");
-
-            if (mainigais < 5)
+            Console.WriteLine(mainigais);
+            // < 
+            // >
+            // ==
+            // !=
+            // <=
+            // >=
+            if (mainigais == 3)
             {
-                Console.WriteLine("mazaks");
+                Console.WriteLine("trijsturis");
             }
             else
             {
-                Console.WriteLine("lielaks");
+                if (mainigais == 4)
+                {
+                    Console.WriteLine("cetrsturis");
+                }
+                else
+                {
+                    if (mainigais == 5)
+                    {
+                        Console.WriteLine("piecsturis");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kluda!");
+                    }
+                }
             }
 
-            
-            
-            if (a > 5)
-            { Console.WriteLine("##"); }
+
+
+
+
+            ievade = Console.ReadLine();
+            mainigais = Convert.ToInt16(ievade);
+
+            switch ( mainigais)
+            {
+                case 1:
+                    Console.WriteLine("George Wasington");
+                    break;
+                case 2:
+                    Console.WriteLine("Thomas Jefferson");
+                    break;
+                case 5:
+                    Console.WriteLine("Abraham Lincoln");
+                    break;
+                case 10:
+                    Console.WriteLine("Alexender Hamilton");
+                    break;
+                case 20:
+                    Console.WriteLine("Andrew Jackson");
+                    break;
+                case 50:
+                    Console.WriteLine("Ulysses S. Grant");
+                    break;
+                case 100:
+                    Console.WriteLine("Benjamin Franklin");
+                    break;
+                default:
+                    Console.WriteLine("Nav tadas valutas!");
+                    break;
+            }
+
+
+
+
+            switch (ievade)
+            {
+                case "piecsturis":
+                    Console.WriteLine("JAAA!!!");
+                    break;
+                default:
+                    Console.WriteLine("NEEE!");
+                    break;
+            }
+
+
+            string mainigais2 = "si ir virkne";
+
+            if (mainigais2 == "si ir virkne")
+            {
+                Console.WriteLine("vienads");
+            }
             else
-            { Console.WriteLine("#"); }
-
-
-
-
-
-
+            {
+                Console.WriteLine("nav vienads");
+            }
 
             Console.ReadLine();
-
-
         }
-        
-
-
-
-
-        
-        
-            
-            
-            
-            
-        
-         
-
-    
-      
-
-        
     }
- }
+}
