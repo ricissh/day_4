f41f4cac3cee1da527b31ad22ae9501487a4a52f
ricissh <rihards.stegmanis@icloud.com>
2018-05-09 20:30:36 +0300

    day_4
    pirmie 2 vardi

--- day_4/day_4/Program.cs
+++ day_4/day_4/Program.cs
@@ -6,7 +6,59 @@ namespace day_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mainigais = 3;
+            string b;
+            int c;
+            int a;
+            b = Console.ReadLine();
+            a = Convert.ToInt16(b);
+           
+
+            mainigais = Convert.ToInt16("5");
+
+            if (mainigais < 5)
+            {
+                Console.WriteLine("mazaks");
+            }
+            else
+            {
+                Console.WriteLine("lielaks");
+            }
+
+            
+            
+            if (a > 5)
+            { Console.WriteLine("##"); }
+            else
+            { Console.WriteLine("#"); }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
         }
+        
+
+
+
+
+        
+        
+            
+            
+            
+            
+        
+         
+
+    
+      
+
+        
     }
-}
+ }
